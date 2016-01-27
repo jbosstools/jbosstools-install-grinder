e147c7eeb5810231c745c1d9678b35a2b0a57db5
nickboldt <nickboldt@gmail.com>
2016-01-27 16:44:48 -0500

    JBIDE-20663 add support for pre-release EPP bundles on hudson.eclipse.org, which don't start with eclipse- (fix regex)

--- scripts/src/main/groovy/getAndInstallEclipse.groovy
+++ scripts/src/main/groovy/getAndInstallEclipse.groovy
@@ -1,6 +1,21 @@
 import groovy.util.AntBuilder
 import org.apache.tools.ant.taskdefs.Get
 
+// this script can install many different variations of and Eclipse installation
+
+// interim EPP bundles
+// groovy -DeclipseCacheDirectory=/tmp/ -DdownloadURL="https://hudson.eclipse.org/packaging/job/mars.epp-tycho-build/325/artifact/org.eclipse.epp.packages/archive/20160121-0844_eclipse-jee-mars-2-RC1-linux.gtk.x86_64.tar.gz" getAndInstallEclipse.groovy
+
+// released EPP bundles (both URL formats supported)
+// groovy -DeclipseCacheDirectory=/tmp/ -DdownloadURL="http://www.eclipse.org/downloads/download.php?r=1&file=/technology/epp/downloads/release/neon/M4/eclipse-jee-neon-M4-linux-gtk-x86_64.tar.gz" getAndInstallEclipse.groovy
+// groovy -DeclipseCacheDirectory=/tmp/ -DdownloadURL="http://download.eclipse.org/technology/epp/downloads/release/mars/1/eclipse-jee-mars-1-linux-gtk.tar.gz" getAndInstallEclipse.groovy
+// groovy -DeclipseCacheDirectory=/tmp/ -DdownloadURL="http://download.eclipse.org/technology/epp/downloads/release/mars/R/eclipse-jee-mars-R-linux-gtk.tar.gz" getAndInstallEclipse.groovy
+
+// Eclipse Platform binaries and SDKs
+// groovy -DeclipseCacheDirectory=/tmp/ -DdownloadURL="http://www.eclipse.org/downloads/download.php?r=1&file=/eclipse/downloads/drops4/S-4.6M4-201512092300/eclipse-platform-4.6M4-linux-gtk-x86_64.tar.gz" getAndInstallEclipse.groovy
+// groovy -DeclipseCacheDirectory=/tmp/ -DdownloadURL="http://www.eclipse.org/downloads/download.php?r=1&file=/eclipse/downloads/drops4/M-4.5.2RC1-201601131000/eclipse-platform-4.5.2RC1-linux-gtk-x86_64.tar.gz" getAndInstallEclipse.groovy
+// groovy -DeclipseCacheDirectory=/tmp/ -DdownloadURL="http://www.eclipse.org/downloads/download.php?r=1&file=/eclipse/downloads/drops4/R-4.5-201506032000/eclipse-SDK-4.5-linux-gtk.tar.gz" getAndInstallEclipse.groovy
+
 Properties allProperties = new Properties()
 allProperties.putAll System.properties
 args.each {
@@ -11,6 +26,8 @@ args.each {
 }
 
 File eclipseCacheDirectory = allProperties['eclipseCacheDirectory'] != null ? new File(allProperties['eclipseCacheDirectory']) : new File(".")
+
+// default when no downloadURL specified is luna SR2 jee EPP bundle
 String eclipseFlavour = allProperties['eclipseFlavour'] ?: "jee"
 String releaseTrainId = allProperties['releaseTrainId'] ?: "luna"
 String versionLabel = allProperties['versionLabel'] ?: "SR2"
@@ -51,8 +68,9 @@ if (osLabel.contains("windows")) {
 String archLabel = System.properties['os.arch'].contains("64") ? "-x86_64" : ""
 
 // if downloadURL is set, just use that value for eclipseArchive and downloadURL
-String eclipseArchive = allProperties['downloadURL'] != null ? allProperties['downloadURL'].replaceAll(".+/(eclipse[^/]+\\."+fileExtension+")",'$1') : "eclipse-" + eclipseFlavour + "-" + releaseTrainId + "-" + (versionLabel == "R" && releaseTrainId[0] < 'k' ? "" : (versionLabel + "-")) + osLabel + archLabel + "." + fileExtension
+String eclipseArchive = allProperties['downloadURL'] != null ? allProperties['downloadURL'].replaceAll(".+/([^/]+\\."+fileExtension+")",'$1') : "eclipse-" + eclipseFlavour + "-" + releaseTrainId + "-" + (versionLabel == "R" && releaseTrainId[0] < 'k' ? "" : (versionLabel + "-")) + osLabel + archLabel + "." + fileExtension
 String downloadURL = allProperties['downloadURL'] ?: mirrorSite + "/" + releaseTrainId + "/" + versionLabel +"/" + eclipseArchive
+
 
 // Support jobs that run on both 32- and 64-bit OS: ensure we're using 64-bit on 64-bit OS
 if (System.properties['os.arch'].contains("64")) {
